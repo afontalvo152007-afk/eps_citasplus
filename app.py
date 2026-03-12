a780991dba4438d79960d43449cbc3c95f7e0267
afontalvo152007-afk <afontalvo152007@gmail.com>
2026-03-12 18:00:55 -0500

    dgd

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ def sedes():
 
 # Ejecutar aplicación
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+ app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
